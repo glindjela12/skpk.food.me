6b472f33cc72b7106454a10ebd1738675942b6df
glindjela12 <105896396+glindjela12@users.noreply.github.com>
2022-05-20 12:29:42 +0700

    first upload

---
+++ database.js
@@ -0,0 +1,103 @@
+let makanan = [];
+
+let makanan_detail = {
+    foto: "https://images.app.goo.gl/yHz7J12VkDbuQJMC6",
+    harga: 15000,
+    kode: "BRG001",
+    nama: "Jus",
+    rating: "5.0",
+    stock: 10,
+} ;
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/6D3sbxu8JWougJJ1A",
+    harga: 15000,
+    kode: "BRG002",
+    nama: "Bakso",
+    rating: "5.0",
+    stock: 20,
+} ;
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/ygbHZuZSdcViAem26",
+    harga: 2000,
+    kode: "BRG003",
+    nama: "Heci",
+    rating: "5.0",
+    stock: 100,
+} ;
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/hXkt26R8t3AnEGwv9",
+    harga: 500,
+    kode: "BRG004",
+    nama: "Permen",
+    rating: "5.0",
+    stock: 20,
+} ;
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/vr7k3ftFDpytKaa49",
+    harga: 6000,
+    kode: "BRG005",
+    nama: "Air mineral",
+    rating: "5.0",
+    stock: 50,
+} ;
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/Swki5vD7aUAdCsHd9",
+    harga: 12000,
+    kode: "BRG006",
+    nama: "Soto ayam",
+    rating: "5.0",
+    stock: 10,
+} ;
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/zMTurJZK5iPRy4m67",
+    harga: 10000,
+    kode: "BRG007",
+    nama: "Rabokki",
+    rating: "5.0",
+    stock: 15,
+} ;
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/WBQYEXRkGwKPadRTA",
+    harga: 7000,
+    kode: "BRG008",
+    nama: "Panecake",
+    rating: "5.0",
+    stock: 8,
+} ;
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/Kc8h7vNR8K2oirgG7",
+    harga: 8000,
+    kode: "BRG009",
+    nama: "cocktail",
+    rating: "5.0",
+    stock: 10,
+} ;
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/Cs5fB2U6ApvQU6Ef8",
+    harga: 7000,
+    kode: "BRG0010",
+    nama: "Teh hijau",
+    rating: "5.0",
+    stock: 20,
+} ;
+makanan.push(makanan_detail)
+
+console.log(makanan)
